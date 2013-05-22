388ea3da96b4235fe6a1b9e6e5da8045bf35b04e
jay.willsson <jay.willsson@googlemail.com>
2013-05-22 10:01:37 +0000

    Initial Upload

---
+++ MEGAMover/qtversion.h
@@ -0,0 +1,16 @@
+/**************************************************************************
+** This file is part of the MEGAMover-Project
+** (C) 2013 Joerg Pauly
+** Author:    joerg
+** Created:   09.05.2013
+**
+** This program is provided under the terms of GNU LGPL.
+**
+** It is provided in the hope that it will be usefull, but with absolutely
+** NO WARRATY.
+**************************************************************************/
+
+#ifndef QTVERSION_H
+#define QTVERSION_H
+#define QT4
+#endif // QTVERSION_H
