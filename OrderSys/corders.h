0d29d0a7ea5ebc24b01229910933837ee19d64c7
jay.willsson@googlemail.com <jay.willsson@googlemail.com@d678e76c-5a12-5ef1-4f74-679254b5aacc>
2013-05-22 12:22:50 +0000

    Initial Upload

---
+++ OrderSys/corders.h
@@ -0,0 +1,62 @@
+/**************************************************************************
+** This file is part of the MEGAMover-Project
+** (C) 2013 Joerg Pauly
+** Author:    joerg
+** Created:   11.05.2013
+**
+** This program is provided under the terms of GNU LGPL.
+**
+** It is provided in the hope that it will be usefull, but with absolutely
+** NO WARRATY.
+**************************************************************************/
+
+#ifndef CORDERS_H
+#define CORDERS_H
+
+#include "qtversion.h"
+#ifdef QT5
+#include <QtWidgets/QWidget>
+#else
+#include <QtGui/QWidget>
+#endif
+
+#include <QMdiSubWindow>
+#include <QSqlQuery>
+#include <QDate>
+
+#include "adresspad.h"
+#include "cnkr.h"
+
+
+namespace Ui {
+class COrders;
+}
+
+class COrders : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit COrders(QWidget *parent = 0);
+    ~COrders();
+    void setSubWindow(QWidget* pSubWnd);
+private slots:
+
+
+    void on_cmdClose_clicked();
+
+    void on_cmdNewAuf_clicked();
+
+private:
+    Ui::COrders *ui;
+    QWidget *m_parent;
+    QWidget *m_subwindow;
+    int m_aufID;
+
+//Private Members
+private:
+    void updateUI();
+
+};
+
+#endif // CORDERS_H
