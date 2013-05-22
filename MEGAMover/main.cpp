736b4c194ba5875d699d1ede0f372fb76d96a2a3
jay.willsson@googlemail.com <jay.willsson@googlemail.com@d678e76c-5a12-5ef1-4f74-679254b5aacc>
2013-05-22 10:06:05 +0000

    Initial Upload

---
+++ MEGAMover/main.cpp
@@ -0,0 +1,24 @@
+/**************************************************************************
+** This file is part of the MEGAMover-Project
+** (C) 2013 Joerg Pauly
+** Author:    joerg
+** Created:   05.05.2013
+**
+** This program is provided under the terms of GNU LGPL.
+**
+** It is provided in the hope that it will be usefull, but with absolutely
+** NO WARRATY.
+**************************************************************************/
+
+
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;    
+    w.showMaximized();
+    
+    return a.exec();
+}
