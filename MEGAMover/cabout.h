50d419b0c0647cb3967ee6f64d7bf658702cd177
jay.willsson <jay.willsson@googlemail.com>
2013-05-22 09:57:10 +0000

    Initial Upload

---
+++ MEGAMover/cabout.h
@@ -0,0 +1,42 @@
+/**************************************************************************
+** This file is part of the MEGAMover-Project
+** (C) 2013 Joerg Pauly
+** Author:    joerg
+** Created:   09.05.2013
+**
+** This program is provided under the terms of GNU LGPL.
+**
+** It is provided in the hope that it will be usefull, but with absolutely
+** NO WARRATY.
+**************************************************************************/
+
+#ifndef CABOUT_H
+#define CABOUT_H
+
+#include "qtversion.h"
+#ifdef QT5
+#include <QtWidgets/QDialog>
+#else
+#include <QtGui/QDialog>
+#endif
+
+namespace Ui {
+class CAbout;
+}
+
+class CAbout : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit CAbout(QWidget *parent = 0);
+    ~CAbout();
+    
+private slots:
+    void on_cmdClose_clicked();
+
+private:
+    Ui::CAbout *ui;
+};
+
+#endif // CABOUT_H
